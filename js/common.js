5d45365d11dd6ffe3b41ab1a42863eddcf7912e4
xmmrh <496249311@qq.com>
2017-10-27 21:18:49 +0800

    vipMovie
    first-update

---
+++ js/common.js
@@ -0,0 +1,81 @@
+function getStyle(obj,attr){
+    if(obj.currentStyle){
+        return obj.currentStyle[attr];
+    }else{
+        return window.getComputedStyle(obj,null)[attr];
+    }
+}
+function client() {
+    if(window.innerWidth != null)  // ie9 +  最新浏览器
+    {
+        return {
+            width: window.innerWidth,
+            height: window.innerHeight
+        }
+    }
+    else if(document.compatMode === "CSS1Compat")  // 标准浏览器
+    {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        }
+    }
+    return {   // 怪异浏览器
+        width: document.body.clientWidth,
+        height: document.body.clientHeight
+    }
+}
+$(function () {
+    user = {
+        iDiv : document.getElementById("iframe_div"),
+        iDivInitHeight : 550,
+        iFramescale : $("#scale_iframe"),
+        iFrameRec : $("#recovery_iframe"),
+        iPlayer : $("#iframe-player"),
+        iInterface : $("#interface_id"),
+        iVip : $("#vip_movie_id"),
+        iButton : $("#happy_id"),
+        smaller : $(".smaller"),
+        enlarge : $(".enlarge"),
+        iHappyfn : function (a,b,c) {
+            console.log(b.val()+c.val());
+            a.attr("src",b.val()+c.val());
+        }
+    }
+
+    if(client().width < 992){
+        var height = Math.ceil(parseInt(getStyle(user.iDiv,"width"))*4.5/5);
+        user.iDiv.style.height = height+"px";
+        user.iDivInitHeight = height;
+    }
+    with(window.location){
+        var vipaddr = search.substring(search.indexOf("=")+1);
+        user.iVip.attr("value",vipaddr);
+        user.iPlayer.attr("src",user.iInterface.eq(0).val()+vipaddr);
+    }
+    with(user){
+        iButton.on("click",function () {
+            iHappyfn(iPlayer,iInterface,iVip);
+        });
+        iFramescale.on("click",function () {
+            var height = iDivInitHeight*0.7;
+            var width = Math.ceil(parseInt(getStyle(iDiv,"width"))*0.7);
+            console.log(height+'==='+iDivInitHeight);
+            iDiv.style.height = height + "px";
+            iDiv.style.backgroundColor = "transparent";
+            iPlayer.attr({"width":width+"px","height":height+"px"});
+            window.scrollTo(0,0);
+            smaller.hide();
+            enlarge.show();
+        });
+        iFrameRec.on("click",function () {
+            iDiv.style.height = iDivInitHeight+"px";
+            iPlayer.attr({"width":"100%","height":iDivInitHeight+"px"});
+            enlarge.hide();
+            smaller.show();
+        });
+    }
+});
+
+
+
